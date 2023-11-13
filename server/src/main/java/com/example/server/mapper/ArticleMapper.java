9dd8bb04e8f17860891f8cd876070b1eac74ae14
ataobiu <ataobiu@ataobiu.outlook.com>
2023-11-14 00:02:06 +0800

    fix

--- server/src/main/java/com/example/server/mapper/ArticleMapper.java
+++ server/src/main/java/com/example/server/mapper/ArticleMapper.java
@@ -7,7 +7,7 @@ import com.example.server.model.Article;
 * @author sherry
 * @description 针对表【article(用户内容)】的数据库操作Mapper
 * @createDate 2023-10-30 22:58:14
-* @Entity com.example.generator.model.Article
+* @Entity com.example.server.model.Article
 */
 public interface ArticleMapper extends BaseMapper<Article> {
 
